9f82961d51c8ed8850600f7bcd3d236c2e699a40
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-05-27 00:35:58 +0300

    Fix: Add HandleCors middleware for Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,23 +8,24 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'vendoractive' => \App\Http\Middleware\vendoractive::class,
-        'checkadmin'=>\App\Http\Middleware\checkadmin::class,
-        'checkcustomer'=>\App\Http\Middleware\checkcustomer::class
-    ]);
-    $middleware->validateCsrfTokens(except: [
-        'api/stripe/webhook',
-    ]);
-    
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->alias([
+            'vendoractive' => \App\Http\Middleware\vendoractive::class,
+            'checkadmin' => \App\Http\Middleware\checkadmin::class,
+            'checkcustomer' => \App\Http\Middleware\checkcustomer::class
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            'api/stripe/webhook',
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-    
+
