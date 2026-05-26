74637d32c1945e06e2e6b8cde552062eee0ae3ec
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-05-27 00:43:46 +0300

    Fix: Add HandleCors middleware for Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->statefulApi();
         $middleware->alias([
             'vendoractive' => \App\Http\Middleware\vendoractive::class,
             'checkadmin' => \App\Http\Middleware\checkadmin::class,
