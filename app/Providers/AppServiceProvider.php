bb118e1b5f6629c930d5ef65aed4b5c7597f7dbc
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-06-01 21:59:09 +0300

    Publish cloudinary config to fix deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // تأكدي إن القيم دي موجودة في الـ Variables بتاعة Railway
+        Config::set('cloudinary.cloud_name', env('CLOUDINARY_CLOUD_NAME'));
+        Config::set('cloudinary.api_key', env('CLOUDINARY_API_KEY'));
+        Config::set('cloudinary.api_secret', env('CLOUDINARY_API_SECRET'));
+
+        // أو لو بتستخدمي الـ URL الموحد
+        Config::set('cloudinary.cloudinary_url', env('CLOUDINARY_URL'));
     }
 }
