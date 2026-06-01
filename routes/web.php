4bd94ac92a4b52293855303278d88d949f9cac4a
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-06-02 02:31:56 +0300

    add temp image upload route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/upload-seed-images', function () {
+    $source = base_path('public/uploads');
+    $files = glob($source . '/*.jpg');
+
+    foreach ($files as $file) {
+        $filename = basename($file);
+        copy($file, public_path('uploads/' . $filename));
+    }
+
+    return 'Done! ' . count($files) . ' images copied.';
 });
