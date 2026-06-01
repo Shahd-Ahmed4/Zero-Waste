79a492f680f7870b5cfc7411071db45bd59f8913
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-06-01 22:34:43 +0300

    Publish cloudinary config to fix deployment

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
-    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    'api_key' => env('CLOUDINARY_API_KEY'),
-    'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'api_environment_variable' => env('CLOUDINARY_URL'),
-    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+    // إضافة الـ URL إذا كنتِ تفضلين استخدامه كبديل
+    'cloudinary_url' => env('CLOUDINARY_URL'),
 ];
