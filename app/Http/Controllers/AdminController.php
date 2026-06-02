7fc597cbeea9501f2f6f4d4fa4782db0020e9f0e
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-06-02 15:22:58 +0300

    fix: capitalize Illuminate in Hash facade import

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,9 @@ use App\Models\vendor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash; // ✅ Illuminate بحرف كبيرclass AdminController extends Controller
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
 {
     /**
      * Display a listing of the resource.
