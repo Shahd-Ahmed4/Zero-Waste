d7953535d357c5007d32db5e3a893976e28161b4
Shahd Ahmed <shahdahmedmohamedsamir@gmail.com>
2026-06-03 08:13:54 +0300

    fix: review image path use asset() instead of storage/

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -124,7 +124,6 @@ class ReviewController extends Controller
             ->latest()
             ->get();
 
-        // 🟢 خطوة إضافية: عشان نضمن إن روابط الصور ترجع كاملة بـ الـ URL بتاع السيرفر للفرونت إند
         $reviews->transform(function ($review) {
             if ($review->image) {
                 $review->image = asset($review->image);
